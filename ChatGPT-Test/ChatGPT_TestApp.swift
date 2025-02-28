2eb6d848ab33fee4061d2c4f3410350b14bcee67
SimeonShaffar <simeonshaffar@gmail.com>
2025-02-28 14:50:52 -0600

    Initial Commit

---
+++ ChatGPT-Test/ChatGPT_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPT_TestApp.swift
+//  ChatGPT-Test
+//
+//  Created by Simeon Shaffar on 2/28/25.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPT_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

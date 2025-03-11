fd0f77952f0434cc7ac75893e221939860ade0a6
SimeonShaffar <simeonshaffar@gmail.com>
2025-03-10 20:50:02 -0500

    Added a button for snapping pictures

--- ChatGPT-Test/Camera/CameraView.swift
+++ ChatGPT-Test/Camera/CameraView.swift
@@ -10,15 +10,57 @@ import SwiftUI
 struct CameraView: View {
     @State private var vm = CameraViewModel()
     
+    @State private var isHolding: Bool = false
+    
     var body: some View {
         GeometryReader { geo in
-            CameraPreview(cameraViewModel: $vm, frame: geo.frame(in: .local))
-                .onAppear() {
-                    vm.requestAccessAndSetup()
-                }
+            ZStack {
+                CameraPreview(cameraViewModel: $vm, frame: geo.frame(in: .local))
+                    .onAppear() {
+                        vm.requestAccessAndSetup()
+                    }
+                
+                VStack {
+                    Spacer()
+                    
+                    button
+                } //VStack
+                
+            } //ZStack
+            
         } .ignoresSafeArea()
         
     }
+    
+    
+    var button: some View {
+        ZStack {
+            Circle() .fill(.white) .frame(width: isHolding ? 80 : 55, height: isHolding ? 80 : 55)
+            
+            Circle() .stroke(.white, lineWidth: 5) .frame(width: 70, height: 70)
+                .padding(20)
+        } //ZStack
+        .padding(.bottom, 50)
+        .gesture(pressGesture)
+    }
+    
+    var pressGesture: some Gesture {
+        DragGesture(minimumDistance: 0)
+            .onChanged { value in
+                withAnimation(.easeOut(duration: 0.1)) {
+                    if !isHolding {
+                        UIImpactFeedbackGenerator(style: .rigid).impactOccurred()
+                        isHolding = true
+                    }
+                } //withAnimation
+            } //onChanged
+            .onEnded { _ in
+                withAnimation(.easeOut(duration: 0.15)) {
+                    UIImpactFeedbackGenerator(style: .soft).impactOccurred()
+                    isHolding = false
+                } //withAnimation
+            } //onEnded
+    } //pressGesture
 }
 
 #Preview {
